d476dc340b1bfe294f91256a71f6d3c9c5089df1
elyrly <elyrly@gmail.com>
2013-11-22 14:51:39 -0800

    video

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,3 +13,18 @@
 //= require jquery
 //= require jquery_ujs
 //= require_tree .
+
+<script type="text/javascript">
+  var index = 1,
+      playlist = ['/video/splash.mp4],
+      video = document.getElementById('awesome_video');
+
+  video.addEventListener('ended', rotate_video, false);
+
+  function rotate_video() {
+    video.setAttribute('src', playlist[index]);
+    video.load();
+    index++;
+    if (index >= playlist.length) { index = 0; }
+  }
+</script>
